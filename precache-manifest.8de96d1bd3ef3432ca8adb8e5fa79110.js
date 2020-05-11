1318efc551fbf9600af00cc9f1cb70300739625e
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-05-12 00:17:46 +0530

    Updates

---
+++ precache-manifest.8de96d1bd3ef3432ca8adb8e5fa79110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fcf231fd0e65274f8116dca2b1b85e",
+    "url": "/React-Dashboard/index.html"
+  },
+  {
+    "revision": "d5fbdc639a5fa3c8e838",
+    "url": "/React-Dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c9ebffb4971ce25a72b",
+    "url": "/React-Dashboard/static/js/2.17faa40b.chunk.js"
+  },
+  {
+    "revision": "ee4ee1ec0345baf3ccdd39c5f31204f6",
+    "url": "/React-Dashboard/static/js/2.17faa40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fbdc639a5fa3c8e838",
+    "url": "/React-Dashboard/static/js/main.30f650c5.chunk.js"
+  },
+  {
+    "revision": "740397b2a8ecb10ab7bf",
+    "url": "/React-Dashboard/static/js/runtime-main.d86664d7.js"
+  }
+]);
